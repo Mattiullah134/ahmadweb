d59f52bbb5feeac992b4b8e1cffe27dbeef3a8b3
Mattiullah <mattikhokhar01@gmail.com>
2024-06-23 18:28:23 +0500

    handle the open and close the dropdown in the navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,3 +1,4 @@
+import { useImperativeHandle, useState } from "react";
 import { NavLink } from "react-router-dom";
 const NavItems = [
   {
@@ -22,6 +23,10 @@ const NavItems = [
   },
 ];
 const NavBar = () => {
+  const [openDropDown, setOpenDropDown] = useState(false);
+  const toggleDropDown = () => {
+    setOpenDropDown(!openDropDown);
+  };
   return (
     <nav className="bg-white z-50 sticky top-0 shadow-sm">
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
@@ -32,6 +37,7 @@ const NavBar = () => {
               className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
               aria-controls="mobile-menu"
               aria-expanded="false"
+              onClick={toggleDropDown}
             >
               <span className="absolute -inset-0.5"></span>
               <span className="sr-only">Open main menu</span>
@@ -101,24 +107,26 @@ const NavBar = () => {
         </div>
       </div>
 
-      <div className="sm:hidden" id="mobile-menu">
-        <div className="space-y-1 px-2 pb-3 pt-2">
-          {NavItems.map((data) => {
-            return (
-              <NavLink
-                to={data.nav}
-                className={(isActive) =>
-                  "rounded-md  px-3 py-2 text-sm font-medium text-blue" +
-                  (isActive ? " underline" : "")
-                }
-                aria-current="page"
-              >
-                {data.name}
-              </NavLink>
-            );
-          })}
+      {openDropDown && (
+        <div id="mobile-menu">
+          <div className="space-y-1 flex flex-col transition-all ease-in-out px-2 pb-3 pt-2">
+            {NavItems.map((data) => {
+              return (
+                <NavLink
+                  to={data.nav}
+                  className={(isActive) =>
+                    "rounded-md  px-3 py-2 text-sm font-medium text-blue" +
+                    (isActive ? " underline" : "")
+                  }
+                  aria-current="page"
+                >
+                  {data.name}
+                </NavLink>
+              );
+            })}
+          </div>
         </div>
-      </div>
+      )}
     </nav>
   );
 };
